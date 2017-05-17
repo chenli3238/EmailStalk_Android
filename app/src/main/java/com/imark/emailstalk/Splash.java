de3a1702e7f0492c5b8a2d75d7809d79dfa583de
Azharaliimark <azhar.ali@imarkinfotech.com>
2017-05-17 15:39:10 +0530

    first commit

---
+++ app/src/main/java/com/imark/emailstalk/Splash.java
@@ -0,0 +1,36 @@
+package com.imark.emailstalk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread t = new Thread() {
+            public void run() {
+                try {
+
+                    sleep(2000);
+                    startActivity(new Intent(Splash.this,LoginActivity.class));
+                    /*if(AppCommon.getInstance(Splash_Activity.this).isUserLogIn()){
+                        startActivity(new Intent(Splash_Activity.this, TabActivity.class));
+                        finish();
+                    }else {
+                        startActivity(new Intent(Splash_Activity.this,LoginActivity.class));
+                        finish();
+                    }*/
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+       t.start();
+    }
+}
