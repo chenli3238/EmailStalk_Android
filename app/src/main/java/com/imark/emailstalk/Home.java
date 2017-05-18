ca3fabf3784aea1196b30c45124e1aba1049871c
Azharaliimark <azhar.ali@imarkinfotech.com>
2017-05-18 19:37:29 +0530

    add all file

--- app/src/main/java/com/imark/emailstalk/Home.java
+++ app/src/main/java/com/imark/emailstalk/Home.java
@@ -3,24 +3,42 @@ package com.imark.emailstalk;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.imark.emailstalk.Adapter.NavigationAdapter;
 import com.imark.emailstalk.Infrastructure.AppCommon;
+import com.imark.emailstalk.Model.NavigationModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import CustomControl.SimpleDividerItemDecoration;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-/**
- * Created by User on 5/18/2017.
- */
+public class Home extends AppCompatActivity{
+    @BindView(R.id.recyclerViewNavigation)
+    RecyclerView recyclerViewNavigation;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.toolbarText)
+    TextView textViewtoolbar;
+    @BindView(R.id.left)
+    ImageView imageViewLeft;
 
-public class Home extends FragmentActivity {
-
+  //////////////////
     @BindView(R.id.readBtn)
     ImageView readBtn;
 
@@ -30,14 +48,47 @@ public class Home extends FragmentActivity {
     @BindView(R.id.allMailBtn)
     ImageView allMailBtn;
     Fragment selectFragment = null;
-
+    private List<NavigationModel> navigationModelArrayList = new ArrayList<>();
+    NavigationAdapter navigationAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_fragment);
+        setContentView(R.layout.home_layout);
         ButterKnife.bind(this);
-        setAllMailBtn();
+        textViewtoolbar.setText(R.string.home);
+        navigationAdapter = new NavigationAdapter(navigationModelArrayList,this);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewNavigation.setLayoutManager(layoutManager);
+        recyclerViewNavigation.setAdapter(navigationAdapter);
+      //  recyclerViewNavigation.addItemDecoration(new SimpleDividerItemDecoration(getApplicationContext(), R.drawable.line_divider_navigation));
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        setUpNavigationdrawer();
+    }
+
+    private void setUpNavigationdrawer() {
+        NavigationModel navigationModel = new NavigationModel(R.drawable.account, "Account");
+        navigationModelArrayList.add(navigationModel);
+
+        navigationModel = new NavigationModel(R.drawable.how, "How it works");
+        navigationModelArrayList.add(navigationModel);
+
+        navigationModel = new NavigationModel(R.drawable.prefference, "Preferences");
+        navigationModelArrayList.add(navigationModel);
+
+        navigationModel = new NavigationModel(R.drawable.faq, "FAQ");
+        navigationModelArrayList.add(navigationModel);
+
+        navigationModel = new NavigationModel(R.drawable.support, "Support");
+        navigationModelArrayList.add(navigationModel);
+
+        navigationModel = new NavigationModel(R.drawable.report, "Report a Bug");
+        navigationModelArrayList.add(navigationModel);
+
     }
 
     @OnClick(R.id.readBtn)
@@ -96,5 +147,13 @@ public class Home extends FragmentActivity {
         transaction.addToBackStack(AllMail.class.getName());
         transaction.commit();
 
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+        }
     }
 }
