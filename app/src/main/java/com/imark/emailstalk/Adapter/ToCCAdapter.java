5a621069abe128e2d1bf21f691b8a0269cf888ce
NavneetImark <navneet.kaur@imarkinfotech.com>
2017-05-27 17:15:34 +0530

    emailDetail

---
+++ app/src/main/java/com/imark/emailstalk/Adapter/ToCCAdapter.java
@@ -0,0 +1,8 @@
+package com.imark.emailstalk.Adapter;
+
+/**
+ * Created by Imark on 5/27/2017.
+ */
+
+public class ToCCAdapter {
+}
---
+++ app/src/main/java/com/imark/emailstalk/EmailDetailActivity.java
@@ -0,0 +1,8 @@
+package com.imark.emailstalk;
+
+/**
+ * Created by Imark on 5/27/2017.
+ */
+
+public class EmailDetailActivity {
+}
